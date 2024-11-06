57aea798c1a9af03657796ce26f3efebac56bd3f
TaharaSuzuho <2211140073b@ed.fuk.kindai.ac.jp>
2024-11-06 08:17:47 +0000

    add congig

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.hosts << "fdc85abbfdca4b0e9cedc1bcc6189a46.vfs.cloud9.us-east-1.amazonaws.com"
 end
