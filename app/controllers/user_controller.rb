1b65f7741ab9f6eab0f82c48354ac700a4866e8b
TaharaSuzuho <2211140073b@ed.fuk.kindai.ac.jp>
2024-11-10 15:25:01 +0000

    profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,44 @@
 class UserController < ApplicationController
+  def index
+    @user = User.all
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to shift_index_path, notice: 'ユーザー情報が更新されました'
+    else
+      render :edit, status: 422
+    end
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    #signup_password = BCrypt::Password.create(params[:user][:pass])
+    u = User.new(uid: params[:user][:uid], password: params[:user][:password],password: params[:user][:password_confirmation])
+    if params[:user][:password] != params[:user][:password_confirmation]
+      render "error", status: 422
+    else
+      u.save
+      redirect_to users_path
+    end
+    
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:uid, :uname, :name, :color, :money)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :user
   resources :shift
   resources :chat
-  
-  get "up" => "rails/health#show", as: :rails_health_check
+
   get 'shift/index'
   root 'shift#index'
 end
